651db1d8c26a0c4b0b3e23f2b5741b72b7562240
John Brown <johnbrownsinbox@gmail.com>
2022-02-23 09:49:22 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpStreet){
+    return Math.abs(pickUpStreet-42);
+
+}
+
+function distanceFromHqInFeet(pickUpStreet){
+    return distanceFromHqInBlocks(pickUpStreet)*264;
+
+}
+
+function distanceTravelledInFeet(pickUpStreet,dropOffStreet){
+    return Math.abs(pickUpStreet-dropOffStreet)*264;
+
+}
+function calculatesFarePrice(pickUpStreet,dropOffStreet){
+   
+    if (distanceTravelledInFeet(pickUpStreet,dropOffStreet)<400) {
+        return(0);
+    }
+    else if(distanceTravelledInFeet(pickUpStreet,dropOffStreet)>=400&&distanceTravelledInFeet(pickUpStreet,dropOffStreet)<=2000){
+        return((distanceTravelledInFeet(pickUpStreet,dropOffStreet)-400)*0.02);
+    }
+    else if(distanceTravelledInFeet(pickUpStreet,dropOffStreet)>2000&&distanceTravelledInFeet(pickUpStreet,dropOffStreet)<=2500){
+        return 25;
+    }
+    else if(distanceTravelledInFeet(pickUpStreet,dropOffStreet)>2500){
+        return 'cannot travel that far';
+    }
+}
